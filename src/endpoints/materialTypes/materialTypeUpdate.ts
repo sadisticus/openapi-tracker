25506e76f993b180ce62c80c86df0f2c7f99c481
sadisticus <and_johnston@hotmail.com>
2026-06-07 21:09:41 +1000

    Edit MaterialTypeUpdate

--- src/endpoints/materialTypes/materialTypeUpdate.ts
+++ src/endpoints/materialTypes/materialTypeUpdate.ts
@@ -5,7 +5,7 @@ import { MaterialType, MaterialTypeModel } from "./base";
 export class MaterialTypeUpdate extends D1UpdateEndpoint<HandleArgs> {
     _meta = {
         model: MaterialTypeModel,
-        fields: MaterialType.scema.pick({
+        fields: MaterialType.schema.pick({
             name: true,
             description: true,
             isActive: true,
